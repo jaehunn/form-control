106f0c7f96a8e7837483deac54eee4e42cffe5eb
Jaehun <qkdwogns98@gmail.com>
2023-03-08 16:50:45 +0900

    uncontrolled

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,73 +1,36 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { FormEvent } from "react";
 import Button from "./Button";
 import TextInput from "./TextInput";
 
 const SignUpForm = () => {
-  const [formValues, setFormValues] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const handleChange = ({
-    target: { name, value },
-  }: ChangeEvent<HTMLInputElement>) => {
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-  };
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log(formValues);
+    const formData = new FormData(e.currentTarget);
+    const values = Object.fromEntries(formData.entries());
+
+    console.log(values);
   };
 
   return (
     <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
       <div className="flex justify-between items-center">
-        <TextInput
-          id="first-name"
-          label="First Name"
-          name="firstName"
-          value={formValues.firstName}
-          onChange={handleChange}
-        />
-
-        <TextInput
-          id="last-name"
-          label="Last Name"
-          name="lastName"
-          value={formValues.lastName}
-          onChange={handleChange}
-        />
+        <TextInput id="first-name" label="First Name" name="firstName" />
+        <TextInput id="last-name" label="Last Name" name="lastName" />
       </div>
 
+      <TextInput id="email " label="Email" name="email" />
       <TextInput
-        id="email "
-        label="Email"
-        name="email"
-        value={formValues.email}
-        onChange={handleChange}
-      />
-
-      <TextInput
+        type="password"
         id="password"
         label="Password"
         name="password"
-        value={formValues.password}
-        onChange={handleChange}
       />
-
       <TextInput
+        type="password"
         id="confirm-password"
         label="Confirm Password"
         name="confirmPassword"
-        value={formValues.confirmPassword}
-        onChange={handleChange}
       />
 
       <div>
